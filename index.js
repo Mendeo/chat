528ceed1530483cffcd38e44bbc228a98c48e318
Themen <thesolve@mail.ru>
2024-06-05 11:22:48 +0300

    Реализовал загрузку файлов.

--- index.js
+++ index.js
@@ -17,10 +17,44 @@ socket.addEventListener('open', ()=>
 			msgInput.value = '';
 		}
 	});
+	const inputFiles = document.getElementById('add-files');
+	inputFiles.addEventListener('change', () =>
+	{
+		for (let f of inputFiles.files)
+		{
+			const r = new FileReader();
+			r.readAsDataURL(f);
+			r.addEventListener('load', () =>
+			{
+				socket.send(`${USER_SESSION_ID}file:${f.name};${r.result}`);
+			});
+		}
+	});
 });
+
+const filesList = document.querySelector('#files-list > ul');
 socket.addEventListener('message', (e)=>
 {
-	chatArea.value += e.data + '\n';
+	const fileStart = e.data.indexOf('file:');
+	let linkStart = -1;
+	if (fileStart !== -1) linkStart = e.data.indexOf(';data:text/xml;base64,', fileStart);
+	if (linkStart !== -1)
+	{
+		const fileName = e.data.slice(fileStart + 5, linkStart);
+		chatArea.value += `${e.data.slice(0, fileStart)}Отправлен файл "${fileName}"\n`;
+		const li = document.createElement('li');
+		const link = document.createElement('a');
+		link.innerText = fileName;
+		link.href = e.data.slice(linkStart + 1);
+		link.download = fileName;
+		if (fileName.length > 20) link.title = fileName;
+		li.append(link);
+		filesList.append(li);
+	}
+	else
+	{
+		chatArea.value += e.data + '\n';
+	}
 });
 socket.addEventListener('error', (e)=>
 {
@@ -61,20 +95,3 @@ for (let i = 0; i < commands.length; i++)
 		submit.click();
 	});
 }
-
-const inputFiles = document.getElementById('add-files');
-const filesList = document.querySelector('#files-list > ul');
-inputFiles.addEventListener('change', () =>
-{
-	for (let f of inputFiles.files)
-	{
-		const li = document.createElement('li');
-		const link = document.createElement('a');
-		link.innerText = f.name;
-		link.href = URL.createObjectURL(f);
-		link.download = f.name;
-		if (f.name.length > 20) link.title = f.name;
-		li.append(link);
-		filesList.append(li);
-	}
-});
